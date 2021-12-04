a26b1aa6eee5b378f719f0db58d4b17a29c5d50a
walt-instructor <staffauthor@outlook.com>
2021-12-03 23:00:02 -0800

    Add globalshere file

---
+++ source/BigStarLib/GlobalsHere.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// put all global usings here.
+// syntax: global using name.space
+
+//namespace BigStarLib
+//{
+//	internal class GlobalsHere
+//	{
+//	}
+//}
