ba5add4506e9e99ac86b170c1b77e36eecf8da65
walt-ritscher <waltritscher@outlook.com>
2021-12-03 21:28:15 -0800

    Add Big Star Collectibles to lib

---
+++ source/BigStarLib/CollectableCard.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BigStarLib.Models
+	
+{
+	public class CollectableCard
+	{
+
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Slogan { get; set; }
+		public int EyeCount { get; set; }
+
+		public Dictionary<string, string> Details { get; set; } = new Dictionary<string, string>();
+
+		public TeamNames TeamName { get; set; }
+
+		public string Description { get; set; }
+		public decimal CatalogPrice { get; set; }
+		public decimal BidPrice { get; set; }
+	
+		public decimal SalePrice
+		{
+			get
+			{
+				if (IsDiscounted)
+				{
+					return BidPrice * .75M;
+				}
+				else
+				{
+					return BidPrice;
+				}
+
+			}
+		}
+		public string ImageUri { get; set; }
+		public string ShortDescription { get; set; }
+		public int PopularityIndex { get; set; }
+
+		public CardFamily CardFamily { get; set; }
+		
+
+		public bool IsDiscounted
+		{
+			get { return PopularityIndex < 450; }
+
+		}
+		
+	}
+	public enum CardFamily
+	{
+		Monsters,
+		Aliens,
+		Robots,
+		Animals
+	}
+	public enum TeamNames
+	{ 
+		BlueShadows,
+		ThunderHeads,
+		SpookTones,
+		Crashmasters,
+		Mavericks,
+		LaserPhasers,
+		Helios,
+		Farsiders,
+		SuperNovas
+	}
+	
+}
