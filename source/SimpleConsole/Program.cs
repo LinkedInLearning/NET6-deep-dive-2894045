6a06c718fde6237fc7cc252870c8204cdc9c6e90
walt-instructor <staffauthor@outlook.com>
2021-12-04 16:10:03 -0800

    Add .NET console and example code

--- source/SimpleConsole/Program.cs
+++ source/SimpleConsole/Program.cs
@@ -1,5 +1,4 @@
-﻿using BigStarLib.Models;
-using System;
+﻿using System;
 
 namespace SimpleConsole
 {
@@ -7,13 +6,41 @@ namespace SimpleConsole
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Some cards from our catalog!");
 
-			var source = new CardSource();
-			foreach (var card in source.CardsOrderedByName)
-			{
-				Console.WriteLine($"{card.SalePrice:C} : {card.Name}");
-			}
+			// detailed explanation at
+			// https://devblogs.microsoft.com/dotnet/string-interpolation-in-c-10-and-net-6/
+
+			// C# compiler rewrites string interpolation in different ways
+			// // depending on the context.
+
+			const string w1 = "Please sign into your account";
+			const string w2 = "Marcus";
+
+			var literalized = $"{w1} {w2}"; // compiler knows these strings are literals
+
+			string pizza1 = "Pepperoni Pizza"; // variable, not literal constant
+			string pizza2 = "Veggie Pizza";
+
+
+			var concatenized = $"Your order of {pizza1} and {pizza2} is ready.";
+			
+			
+			int x = 5; int y = 7;
+			var s1 = $"Your numbers are {x} and {y}";
+
+			// compiler changes to:
+			
+			var s2 = String.Format("Your number are {0} and {1}", x, y);
+			// this causes value type boxing and other performance concerns! 
+			Console.WriteLine(s1);
+			;
+			NewInterpolation();
+		}
+		public static void NewInterpolation()
+		{
+			int dice1 = 3; int dice2 = 72;
+			var message = $"Dice results: {dice1} and {dice2}.  Total = {dice1 + dice2}";
+
 		}
 	}
 }
---
+++ source/SimpleConsoleNet6/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+NewInterpolation();
+
+ static void NewInterpolation()
+{
+	int dice1 = 3; int dice2 = 72;
+	var s1 = $"Dice results: {dice1} and {dice2}.  Total = {dice1 + dice2}";
+}
+
+static void WithStringBuilder()
+{
+	int dice1 = 3; int dice2 = 72;
+	var s1 = $"Dice results: {dice1} and {dice2}.  Total = {dice1 + dice2}";
+	var builder = new StringBuilder ();
+	builder.Append("Dice results: ");
+	builder.Append($"{dice1} and {dice2}. ");
+	builder.Append($"Total = {dice1 + dice2}")
+}
