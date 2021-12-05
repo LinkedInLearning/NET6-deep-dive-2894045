5445909ea986b81d1efaef1bfbd7482d0b489ea7
walt-instructor <staffauthor@outlook.com>
2021-12-04 17:40:21 -0800

    Refactor code in .NET project

--- source/SimpleConsoleNet5/Program.cs
+++ source/SimpleConsoleNet5/Program.cs
@@ -13,34 +13,9 @@ namespace SimpleConsole
 			// C# compiler rewrites string interpolation in different ways
 			// // depending on the context.
 
-			const string w1 = "Please sign into your account";
-			const string w2 = "Marcus";
-
-			var literalized = $"{w1} {w2}"; // compiler knows these strings are literals
-
-			string pizza1 = "Pepperoni Pizza"; // variable, not literal constant
-			string pizza2 = "Veggie Pizza";
-
-
-			var concatenized = $"Your order of {pizza1} and {pizza2} is ready.";
-
-			//var concat2 = String.Concat();
-			int x = 5; int y = 7;
-			var s1 = $"Your numbers are {x} and {y}";
-
-			// compiler changes to:
-			
-			var s2 = String.Format("Your number are {0} and {1}", x, y);
-			// this causes value type boxing and other performance concerns! 
-			Console.WriteLine(s1);
-			;
-			NewInterpolation();
+		// see examples in class files in this project
+		// and the .NET 6 project
 		}
-		public static void NewInterpolation()
-		{
-			int dice1 = 3; int dice2 = 72;
-			var message = $"Dice results: {dice1} and {dice2}.  Total = {dice1 + dice2}";
-
-		}
+	
 	}
 }
--- source/SimpleConsoleNet6/Program.cs
+++ source/SimpleConsoleNet6/Program.cs
@@ -11,12 +11,3 @@ NewInterpolation();
 	var s1 = $"Dice results: {dice1} and {dice2}.  Total = {dice1 + dice2}";
 }
 
-static void WithStringBuilder()
-{
-	int dice1 = 3; int dice2 = 2;
-	var s1 = $"Dice results: {dice1} and {dice2}.  Total = {dice1 + dice2}";
-	var builder = new StringBuilder ();
-	builder.Append("Dice results: ");
-	builder.Append($"{dice1} and {dice2}. ");
-	builder.Append($"Total = {dice1 + dice2}");
-}
