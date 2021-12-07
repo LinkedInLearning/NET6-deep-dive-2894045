29c75e65252430de596699c526c1c0751d72aec4
walt-instructor <staffauthor@outlook.com>
2021-12-07 00:36:50 -0800

    Remove comments

--- source/DataLib/ToDoRepository.cs
+++ source/DataLib/ToDoRepository.cs
@@ -5,29 +5,27 @@ namespace DataLib.Data {
 	public class ToDoRepository {
 
 		public ToDoRepository() {
-			ToDoItem toDo = new ToDoItem { Id=1, IsComplete= false, Name= "Send soundtrack proposal to Hans." };
-			_toDoList.Add(toDo.Id, toDo);
-			toDo = new ToDoItem { Id = 2, IsComplete = true, Name = "Bake whole wheat bread" };
-			_toDoList.Add(toDo.Id, toDo);
-			toDo = new ToDoItem { Id = 3, IsComplete = false, Name = "Buy guitar strings" };
-			_toDoList.Add(toDo.Id, toDo);
-			toDo = new ToDoItem { Id = 4, IsComplete = false, Name = "Record backing vocals" };
-			_toDoList.Add(toDo.Id, toDo);
+			ToDoItem toDoItem = new ToDoItem { Id = 1, IsComplete = false, Name = "Send soundtrack proposal to Hans." };
+			_toDoList.Add(toDoItem.Id, toDoItem);
+			toDoItem = new ToDoItem { Id = 2, IsComplete = true, Name = "Bake whole wheat bread" };
+			_toDoList.Add(toDoItem.Id, toDoItem);
+			toDoItem = new ToDoItem { Id = 3, IsComplete = false, Name = "Buy guitar strings" };
+			_toDoList.Add(toDoItem.Id, toDoItem);
+			toDoItem = new ToDoItem { Id = 4, IsComplete = false, Name = "Record backing vocals" };
+			_toDoList.Add(toDoItem.Id, toDoItem);
 		}
-	//	private readonly ConcurrentDictionary<long, ToDoItem> _toDoListOld = new();	
+
 
 		private readonly Dictionary<long, ToDoItem> _toDoList = new();
-    public void Create(ToDoItem item)
-    {
-      if (_toDoList.ContainsKey(item.Id))
-      {
-				var highestId = _toDoList.Max(x=>x.Value.Id) +1;
+		public void Create(ToDoItem item) {
+			if (_toDoList.ContainsKey(item.Id))
+			{
+				var highestId = _toDoList.Max(x => x.Value.Id) + 1;
 				item.Id = highestId;
-      }
-			_toDoList.Add(item.Id,item);
-    }
-		public void Update(ToDoItem item)
-		{
+			}
+			_toDoList.Add(item.Id, item);
+		}
+		public void Update(ToDoItem item) {
 			if (_toDoList.ContainsKey(item.Id))
 			{
 				_toDoList[item.Id] = item;
@@ -35,7 +33,7 @@ namespace DataLib.Data {
 			}
 		}
 		public void Delete(long id) {
-		_toDoList.Remove(id);
+			_toDoList.Remove(id);
 		}
 		public List<ToDoItem> GetAll() {
 			return _toDoList.Values.ToList();
