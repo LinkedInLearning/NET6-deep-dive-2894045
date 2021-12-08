a1b0e146194b46a067c40e3fa3fac1a1e5e06b8e
walt-instructor <staffauthor@outlook.com>
2021-12-07 22:56:08 -0800

    add examples

---
+++ source/SimpleConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Details at https://devblogs.microsoft.com/dotnet/date-time-and-time-zone-enhancements-in-net-6/
+// DateOnly
+
+var d1 = new DateOnly();
+
+var d2 = DateOnly.FromDateTime(DateTime.Now);
+
+d2 = d2.AddDays(10);
+
+var d3 = new DateOnly( year:2025,month:11,day:20);
+
+// TimeOnly
+var t1 = new TimeOnly();
+
+var t2 = TimeOnly.FromDateTime(DateTime.Now);
+
+var t3 = new TimeOnly(hour: 11,minute: 17, second:45);
+	
