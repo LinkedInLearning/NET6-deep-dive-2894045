730a635b39f49887f4db1853a3d977066de0de08
walt-instructor <staffauthor@outlook.com>
2021-12-07 23:13:12 -0800

    add example code

--- source/SimpleConsole/Program.cs
+++ source/SimpleConsole/Program.cs
@@ -1,21 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
-// Details at https://devblogs.microsoft.com/dotnet/date-time-and-time-zone-enhancements-in-net-6/
-// DateOnly
 
-var d1 = new DateOnly();
+DoWork("hello", 5);
+DoBetterWork("goodbye", 7);
+// DoBetterWork(null, 7);
 
-var d2 = DateOnly.FromDateTime(DateTime.Now);
 
-d2 = d2.AddDays(10);
+static void DoWork(string message, int count) {
+	if (message is null)
+	{
+		throw new ArgumentNullException(nameof(message));
+	}
 
-var d3 = new DateOnly( year:2025,month:11,day:20);
+	for (int counter = 0; counter < count; counter++)
+	{
+		Console.WriteLine(message);
+	}
+}
 
-// TimeOnly
-var t1 = new TimeOnly();
+static void DoBetterWork(string message, int count) {
+	ArgumentNullException.ThrowIfNull(message);
+	// ArgumentNullException.ThrowIfNull(message);
 
-var t2 = TimeOnly.FromDateTime(DateTime.Now);
+	for (int counter = 0; counter < count; counter++)
+	{
+		Console.WriteLine(message);
+	}
+}
 
-var t3 = new TimeOnly(hour: 11,minute: 17, second:45);
-	
+
+
