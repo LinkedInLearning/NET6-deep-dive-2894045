053dc0828c9bced6a359962168dd23b268d3c312
walt-instructor <staffauthor@outlook.com>
2021-12-07 16:34:27 -0800

    add page level css

---
+++ source/SimpleBlazorApp/Pages/Privacy.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SimpleBlazorApp.Pages {
+	public class AboutModel : PageModel {
+		private readonly ILogger<PrivacyModel> _logger;
+
+		public AboutModel(ILogger<PrivacyModel> logger) {
+			_logger = logger;
+		}
+
+		public void OnGet() {
+		}
+	}
+}
