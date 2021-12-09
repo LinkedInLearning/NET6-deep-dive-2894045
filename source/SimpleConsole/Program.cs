86724c9475acfa666330e1b77865cc1c87a632cf
walt-ritscher <waltritscher@outlook.com>
2021-12-09 14:54:17 -0800

    add examples

--- source/SimpleConsole/Program.cs
+++ source/SimpleConsole/Program.cs
@@ -4,28 +4,66 @@ class Program
 {
   static void Main()
   {
-    //Example1();
-    //Example2();
-    Example3();
+    Example1();
+    Example2();
+    //Example3();
 
   }
   static void Example1()
   {
+    Console.WriteLine("Example 1 - Priority Queue");
+    // a new class PriorityQueue
+    // each queue item is assigned a priority level when added to the queue.
+    // specify a priority type when creating the PriorityQueue
+    // highest priority (lower number) items are removed first
+    // note: is not threadsafe
 
+    var foodQueue = new PriorityQueue<string, int>();
+
+    foodQueue.Enqueue("apple", 9);
+    foodQueue.Enqueue("banana", 5);
+    foodQueue.Enqueue("cherry", 5);
+    foodQueue.Enqueue("durian", 4);
+    foodQueue.Enqueue("eggplant", 9);
+
+    // cannot enumerate it with a loop 
+    // foreach(var food in foodQueue) 
+    while (foodQueue.TryDequeue(out var food, out var level)) {
+      Console.WriteLine($"{food}, {level}");
+    }
+   // note, cannot change priority one item is enqueued.
 
   }
   static void Example2()
   {
+    Console.WriteLine("Example 2 - Set capacity");
 
+
+    // Call EnsureCapacity() before inserting large set of data into collections
+    // This allows the collection to determine memory size early.
+    // Avoids resizing internal memory as items are added.
+
+    var myList = new List<int>();
+    var myStack = new Stack<int>();
+    var myQueue = new Queue<int>();
+    var myList2 = new List<int>(capacity:300);
+
+ 
+    myList.EnsureCapacity(500);
+
+    myStack.EnsureCapacity(500);
+    myQueue.EnsureCapacity(500);
+
+    // dictionaries and hashset already had this method
+
+    var myHashSet = new HashSet<int>();
+    myHashSet.EnsureCapacity(500);
+
+    C
 
   }
 
-  static void Example3()
-  {
-    
 
-
-  }
 
 
 
