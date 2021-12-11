700b01d71c79dcd51a99e2bafe5f3607e38116d5
walt-ritscher <waltritscher@outlook.com>
2021-12-10 21:51:20 -0800

    add mutable DOM example

--- source/SimpleConsole/Program.cs
+++ source/SimpleConsole/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Linq;
 using System.Text.Json;
+using System.Text.Json.Nodes;
 
 namespace SimpleConsole;
 class Program
@@ -8,27 +10,48 @@ class Program
   static async Task Main()
   {
 
-   List<Food> foods=new List<Food>();
-   foods.Add (new Food { FoodName = "banana", Calories = 130, Category= "Fruit" });
-    foods.Add (new Food { FoodName = "fig", Calories = 245, Category = "Fruit" });
+    List<Food> foods = new List<Food>();
+    foods.Add(new Food { FoodName = "banana", Calories = 130, Category = "Fruit" });
+    foods.Add(new Food { FoodName = "fig", Calories = 245, Category = "Fruit" });
 
     var x = new FoodGenerationContext();
-    byte[] utf8Json = JsonSerializer.SerializeToUtf8Bytes( foods[0],                                                         FoodGenerationContext.Default.Food);
+    byte[] utf8Json = JsonSerializer.SerializeToUtf8Bytes(foods[0], FoodGenerationContext.Default.Food);
 
 
     Console.WriteLine(utf8Json);
-    var stringFood = JsonSerializer.Serialize( foods[1],
+    var stringFood = JsonSerializer.Serialize(foods[1],
                                               FoodGenerationContext.Default.Food);
 
     Console.WriteLine(stringFood);
 
     var foodAgain = JsonSerializer.Deserialize(stringFood, FoodGenerationContext.Default.Food);
 
-		var stringFoods = JsonSerializer.Serialize(foods,
-																							 FoodsGenerationContext.Default.IEnumerableFood);
+    var stringFoods = JsonSerializer.Serialize(foods,
+                                     FoodsGenerationContext.Default.IEnumerableFood);
+
+    Console.WriteLine("Original JSON");
     Console.WriteLine(stringFoods);
 
 
+    // use writeable DOM to modify a node
+    var topNode = JsonNode.Parse(stringFoods);
+
+
+    // The DOM lets you navigate to a subsection of a JSON payload
+    // and deserialize a single value, a custom type, or an array.
+    var foodNode = topNode[0];
+    foodNode["foodName"] = "Eggplant";
+
+
+    var modifiedFoods = JsonSerializer.Deserialize(topNode,
+                                    FoodsGenerationContext.Default.IEnumerableFood);
+
+
+    Console.WriteLine("Modified DOM");
+    Console.WriteLine(topNode);
+
+
+    Console.WriteLine((modifiedFoods.First()as Food).FoodName);
   }
 
 
